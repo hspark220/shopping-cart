f39f884752776bd91f0d55a1c24e65fa6ad130d1
Heon Soo Park <hspark220@gmail.com>
2023-04-25 08:05:54 -0400

    Add products

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,10 +1,26 @@
+import React, { useState } from "react";
 import Navigation from "../components/Navigation";
+import "../css/products.css"
 
 const ShoppingCart = () => {
+    const [products, setProducts] = useState([
+        1,2,3,4,5,6,7,8,9,10
+    ])
+
     return (
         <div className="shopping-page">
             <h1>Shoppage</h1>
             <Navigation />
+            <div className="products">
+                {products.map(product => {
+                    return (
+                        <div className="product">
+                            <h1>{product}</h1>
+                        </div>
+                    )
+                })}
+            </div>
+            
         </div>
     )
 };
